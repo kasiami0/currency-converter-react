90b6a26800e73401087c98c6c74f16a8729f439a
Katarzyna Migacz <k.migacz94@gmail.com>
2023-02-16 14:24:18 +0100

    currency array

---
+++ src/currencies.js
@@ -0,0 +1,8 @@
+const currencies = [
+    { id: 1, name: "Dolar Amerykański", code: "USD", rate: 4.5337 },
+    { id: 2, name: "Euro", code: "EUR", rate: 4.7067 },
+    { id: 3, name: "Frank Szwajcarski", code: "CHF", rate: 4.7675 },
+    { id: 4, name: "Funt Brytyjski", code: "GBP", rate: 5.4075 },
+];
+
+export {currencies};
