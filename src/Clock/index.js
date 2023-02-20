8de58ec9c340e93240bff6d3df97e82249bce0d5
Katarzyna Migacz <k.migacz94@gmail.com>
2023-02-20 18:28:54 +0100

    dateFormat moved

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -1,17 +1,17 @@
 import { useEffect, useState } from "react";
 import "./style.css"
 
+const dateFormat = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+};
+
 const Clock = () => {
     const [date, setDate] = useState(new Date());
-
-    const dateFormat = {
-        weekday: "long",
-        day: "numeric",
-        month: "long",
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit",
-    };
 
     useEffect(() => {
         const intervalId = setInterval(() => {
